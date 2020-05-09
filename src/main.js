f80683aec61d91d576d96d978708105574209446
xiaomeihao <317991755@qq.com>
2020-05-09 11:02:54 +0800

    项目初始化 移动端rem适配配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载vant组件库
+import Vant from 'vant'
+// 加载vant组件库样式
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+// 全局注册vant中的组件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
