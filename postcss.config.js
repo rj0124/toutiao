b060f08df34a33570600c8f44bb515b33953167e
xiaomeihao <317991755@qq.com>
2020-05-09 11:49:50 +0800

    关于autoprefixer的配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
+// Postcss 是基于Node.js 运行的处理css的工具,它的配置文件也是运行在Node.js中
+
+// Postcss配置文件需要导出一个对象
 module.exports = {
   plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 8']
-    },
+    // autoprefixer: {
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
     'postcss-pxtorem': {
       rootValue: 37.5,
       propList: ['*']
