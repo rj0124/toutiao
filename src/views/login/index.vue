d80a74455667cf5736c49711da9d1b730ea1d113
xiaomeihao <317991755@qq.com>
2020-05-10 10:34:51 +0800

    完成表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,12 +8,13 @@
     />
 
     <!-- 登录表单 -->
-    <van-cell-group>
+    <van-form @submit="onLogin">
       <van-field
         v-model="user.mobile"
         icon-prefix="iconfont icon"
         left-icon="shouji"
         placeholder="请输入手机号"
+        :rules="fromRules.mobile"
       />
       <van-field
         v-model="user.code"
@@ -21,21 +22,23 @@
         icon-prefix="iconfont icon"
         left-icon="yanzhengma"
         placeholder="请输入验证码"
+        :rules="fromRules.code"
       >
       <template #button>
         <van-button class="send-btn" size="small" round>发送验证码</van-button>
       </template>
       </van-field>
-    </van-cell-group>
-    <div class="login-btn-wrap">
-      <van-button class="login-btn" type="info" block @click="onLogin">登录</van-button>
-    </div>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" type="info" block>登录</van-button>
+      </div>
+    </van-form>
   </div>
 </template>
 
 <script>
 
 import { login } from '@/api/user'
+import { Toast } from 'vant'
 
 export default {
   name: 'LoginIndex',
@@ -48,6 +51,16 @@ export default {
         mobile: '',
         // 验证码
         code: ''
+      },
+      fromRules: {
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '手机号格式错误' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码格式错误' }
+        ]
       }
     }
   },
@@ -57,6 +70,14 @@ export default {
   mounted () {},
   methods: {
     async onLogin () {
+      Toast.loading({
+        // 提示文本
+        message: '登录中...',
+        // 禁止背景点击
+        forbidClick: true,
+        // 展示时长
+        duration: 0
+      })
       // 1.找到数据接口
       // 2.封装请求方法
       // 3.请求调用登录
@@ -64,9 +85,10 @@ export default {
         const res = await login(this.user)
         // 4.处理响应结果
         console.log(res)
+        Toast.success('登录成功')
       } catch (err) {
         console.log(err)
-        console.log('登录失败', err)
+        Toast.fail('登录失败,手机号或验证码错误')
       }
     }
   }
