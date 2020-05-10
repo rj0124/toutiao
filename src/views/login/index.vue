4182bc11abc46640fb1cefaf3d7ee60ae6775ee3
xiaomeihao <317991755@qq.com>
2020-05-10 12:35:09 +0800

    完成短信验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,36 +2,69 @@
   <div class="login-container">
     <van-nav-bar
       class="app-nav-bar"
-      title="登录 / 注册"
+      title="注册 / 登录"
       left-arrow
       @click-left="$router.back()"
     />
-
     <!-- 登录表单 -->
-    <van-form validate-firsts :show-error="false" :show-error-message="false" @submit="onLogin" @failed="onFailed">
+    <!--
+      基于 Vant 的表单验证：
+      1、使用 van-form 组件包裹所有的表单项 van-field
+      2、给 van-form 绑定 submit 事件
+         当表单提交的时候会触发 submit 事件
+         提示：只有表单验证通过，它才会调用 submit
+      3、使用 Field 的rules属性定义校验规则
+     -->
+    <van-form
+      :show-error="false"
+      :show-error-message="false"
+      validate-first
+      ref="login-form"
+      @submit="onLogin"
+      @failed="onFailed"
+    >
       <van-field
         v-model="user.mobile"
         icon-prefix="iconfont icon"
         left-icon="shouji"
+        center
         placeholder="请输入手机号"
-        name="手机号"
-        :rules="fromRules.mobile"
+        name="mobile"
+        :rules="formRules.mobile"
       />
       <van-field
         v-model="user.code"
         clearable
         icon-prefix="iconfont icon"
         left-icon="yanzhengma"
+        center
         placeholder="请输入验证码"
-        name="验证码"
-        :rules="fromRules.code"
+        name="code"
+        :rules="formRules.code"
       >
-      <template #button>
-        <van-button class="send-btn" size="small" round>发送验证码</van-button>
-      </template>
+        <template #button>
+          <van-count-down
+            v-if="isCountDownShow"
+            :time="1000 * 60"
+            format="ss s"
+            @finish="isCountDownShow = false"
+          />
+          <van-button
+            v-else
+            class="send-btn"
+            size="mini"
+            round
+            :loading="isSendSmsLoading"
+            @click.prevent="onSendSms"
+          >发送验证码</van-button>
+        </template>
       </van-field>
       <div class="login-btn-wrap">
-        <van-button class="login-btn" type="info" block>登录</van-button>
+        <van-button
+          class="login-btn"
+          type="info"
+          block
+        >登录</van-button>
       </div>
     </van-form>
   </div>
@@ -39,7 +72,7 @@
 
 <script>
 
-import { login } from '@/api/user'
+import { login, sendSms } from '@/api/user'
 // import { Toast } from 'vant'
 
 export default {
@@ -50,11 +83,11 @@ export default {
     return {
       user: {
         // 手机号
-        mobile: '',
+        mobile: '17090086870',
         // 验证码
         code: ''
       },
-      fromRules: {
+      formRules: {
         mobile: [
           { required: true, message: '请输入手机号' },
           { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '手机号格式错误' }
@@ -63,7 +96,11 @@ export default {
           { required: true, message: '请输入验证码' },
           { pattern: /^\d{6}$/, message: '验证码格式错误' }
         ]
-      }
+      },
+      // 控制倒计时和发送按钮的显示状态
+      isCountDownShow: false,
+      // 发送验证码按钮的 loading 状态
+      isSendSmsLoading: false
     }
   },
   computed: {},
@@ -77,34 +114,70 @@ export default {
         message: '登录中...',
         // 禁止背景点击
         forbidClick: true,
-        // 展示时长
+        // 展示时长(ms)，值为 0 时，toast 不会消失
         duration: 0
       })
-      // 1.找到数据接口
-      // 2.封装请求方法
-      // 3.请求调用登录
+      // 1. 找到数据接口
+      // 2. 封装请求方法
+      // 3. 请求调用登录
       try {
         const res = await login(this.user)
-        // 4.处理响应结果
+        // 4. 处理响应结果
         console.log(res)
         this.$toast.success('登录成功')
       } catch (err) {
         console.log(err)
-        this.$toast.fail('登录失败,手机号或验证码错误')
+        this.$toast.fail('登录失败，手机号或验证码错误')
       }
     },
     onFailed (error) {
-      console.log('验证失败', error)
       if (error.errors[0]) {
         this.$toast({
+          // 提示消息
           message: error.errors[0].message,
+          // 防止键盘太高看不见提示消息
           position: 'top'
         })
       }
+    },
+    async onSendSms () {
+      try {
+        // 校验手机号码
+        await this.$refs['login-form'].validate('mobile')
+        // 验证通过，请求发送验证码
+        // 展示按钮的 loading 状态，防止网络慢用户多次点击触发发送行为
+        this.isSendSmsLoading = true
+        await sendSms(this.user.mobile)
+        // 短信发送，隐藏发送按钮，显示倒计时
+        this.isCountDownShow = true
+        // 倒计时结束 -> 隐藏倒计时，显示发送按钮（监视倒计时的 finish 事件处理）
+      } catch (err) {
+        // try 里面任何代码的错误都会进入 catch
+        // 不同的错误需要有不同的提示，那就需要判断了
+        let message = ''
+        if (err && err.response && err.response.status === 429) {
+          // 发送短信失败的错误提示
+          message = '发送太频繁了，请稍后重试'
+        } else if (err.name === 'mobile') {
+          // 表单验证失败的错误提示
+          message = err.message
+        } else {
+          // 未知错误
+          message = '发送失败，请稍后重试'
+        }
+        // 提示用户
+        this.$toast({
+          message,
+          position: 'top'
+        })
+      }
+      // 无论发送验证码成功还是失败，最后都要关闭发送按钮的 loading 状态
+      this.isSendSmsLoading = false
     }
   }
 }
 </script>
+
 <style scoped lang="less">
 .login-container {
   .send-btn {
@@ -113,13 +186,13 @@ export default {
     background-color: #ededed;
     .van-button__text {
       font-size: 11px;
-      color: #666;
+      color: #666666;
     }
   }
   .login-btn-wrap {
     padding: 26px 16px;
     .login-btn {
-      background-color: #6bd4fb;
+      background-color: #6db4fb;
       border: none;
       .van-button__text {
         font-size: 15px;
